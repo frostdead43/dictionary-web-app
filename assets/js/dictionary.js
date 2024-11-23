2345358a99f2c0b674d459778c858f2bc67d510c
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2024-11-23 23:46:38 +0300

    completed

---
+++ assets/js/dictionary.js
@@ -0,0 +1,78 @@
+const inputSearch = document.querySelector(".search-bar");
+const contentArea = document.querySelector(".content");
+
+async function getFetch() {
+  
+const dictionaryData = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputSearch.value}`).then(res => res.json());
+
+contentArea.innerHTML = "";
+
+for (const data of dictionaryData) {
+
+  contentArea.innerHTML += `
+      <div class="search-area">
+        <div>
+          <h2>${data.word}</h2>
+          <span>${data.phonetic || "No phonetic available"}</span>
+        </div>
+        <div class="run">
+          <button><img src="assets/images/run-icon.svg"></button>
+        </div>
+      </div>
+      `
+ 
+      for (const meaning of data.meanings) {
+        contentArea.innerHTML += `
+          <h6>${meaning.partOfSpeech}</h6>
+          <ul>Meaning</ul>
+        `;
+      for (const definition of meaning.definitions) {
+
+        contentArea.innerHTML += `<li>${definition.definition}</li>`;
+
+        if (definition.example) {
+          contentArea.innerHTML += `<p>“${definition.example}”</p>`;
+        }
+      }
+      if (meaning.synonyms && meaning.synonyms.length > 0) {
+        contentArea.innerHTML += `<div class="weird-area"><span>Synonyms:</span>`;
+        for (const synonym of meaning.synonyms) {
+          contentArea.innerHTML += `<a class = "synonyms-btn">${synonym}</a> , </div>`;
+        }
+      const anchorBtns = contentArea.querySelectorAll(".synonyms-btn");
+      for (const btn of anchorBtns) {
+        btn.addEventListener("click",function() {
+          inputSearch.value = btn.innerText;
+          getFetch();
+          console.log("tiklandi");
+
+        })
+      }
+  //     for (const url of data.sourceUrls) {
+  //       contentArea.innerHTML += `<footer>
+  //    <h5>Source</h5> 
+  //    <a href="${url}">${url}</a> 
+  // </footer>`
+  //     }
+      }
+  
+    }
+  break;
+  }
+}
+
+function getFormData() {
+  formId.addEventListener("submit", function(e) {
+    e.preventDefault();
+    console.log("form gönderildi");
+    getFetch();
+  })
+}
+
+getFormData();
+
+  
+
+
+
+
