1693e76ca2e658714dc588546f517b912f495117
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2024-11-27 15:11:57 +0300

    gökdeniz hatayı fixledi

--- assets/js/dictionary.js
+++ assets/js/dictionary.js
@@ -3,14 +3,14 @@ const contentArea = document.querySelector(".content");
 const footerArea = document.querySelector(".footer-area");
 const switchButton = document.querySelector(".switch");
 
+const audioElement = document.createElement("audio");
+audioElement.classList.add("hidden");
+document.body.appendChild(audioElement);
+
 async function getFetch() {
-  
-const dictionaryData = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputSearch.value}`).then(res => res.json());
-
-contentArea.innerHTML = "";
-
-for (const data of dictionaryData) {
-
+  const dictionaryData = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputSearch.value}`).then(res => res.json());
+  contentArea.innerHTML = "";
+  for (const data of dictionaryData) {
   contentArea.innerHTML += `
       <div class="search-area">
         <div>
@@ -22,14 +22,12 @@ for (const data of dictionaryData) {
         </div>
       </div>
       `
- 
       for (const meaning of data.meanings) {
         contentArea.innerHTML += `
           <h6>${meaning.partOfSpeech}</h6>
           <ul>Meaning</ul>
         `;
       for (const definition of meaning.definitions) {
-
         contentArea.innerHTML += `<li>${definition.definition}</li>`;
 
         if (definition.example) {
@@ -41,41 +39,72 @@ for (const data of dictionaryData) {
         for (const synonym of meaning.synonyms) {
           contentArea.innerHTML += `<a class = "synonyms-btn">${synonym}</a> , </div>`;
         }
-      const anchorBtns = contentArea.querySelectorAll(".synonyms-btn");
-      for (const btn of anchorBtns) {
-        btn.addEventListener("click",function() {
-          inputSearch.value = btn.innerText;
-          getFetch();
-          console.log("tiklandi");
+        for (const url of data.sourceUrls) {
+          footerArea.innerHTML = 
+          `
+          <h5>Source</h5> 
+          <a href="${url}">${url}</a> `
+        }
+      // const anchorBtns = contentArea.querySelectorAll(".synonyms-btn");
+      // for (const btn of anchorBtns) {
+      //   btn.addEventListener("click",function() {
+      //     inputSearch.value = btn.innerText;
+      //     getFetch();
+      //     console.log("tiklandi");
 
-        })
-      }
-    }
-    for (const url of data.sourceUrls) {
-      footerArea.innerHTML = 
-      `
-      <h5>Source</h5> 
-      <a href="${url}">${url}</a> `
+      //   })
+      // }
     }
 
     //sorun burada
-    for (const phonetic of data.phonetics) {
+    // for (const phonetic of data.phonetics) {
 
-      if (phonetic.audio) {
-        const runBtns = contentArea.querySelectorAll(".run-btn");
-        for (const btn of runBtns) {
-          btn.addEventListener("click",function() {
-           contentArea.innerHTML += `<audio class ="hidden" controls autoplay src="${phonetic.audio}"></audio>`;
-        })
-      }
+    //   if (phonetic.audio) {
+    //     const runBtns = contentArea.querySelectorAll(".run-btn");
+    //     for (const btn of runBtns) {
+    //       btn.addEventListener("click",function() {
+    //        contentArea.innerHTML += `<audio class ="hidden" controls autoplay src="${phonetic.audio}"></audio>`;
+    //     })
+    //   }
     
-    }
+    // }
   
   }
+  setupRunButtons(data.phonetics);
+  setupSynonymButtons();
   break;
   }
 }
+
+//chat gpt bölümü
+function setupRunButtons(phonetics) {
+  const runBtns = contentArea.querySelectorAll(".run-btn");
+  runBtns.forEach((btn, index) => {
+    btn.addEventListener("click", function () {
+      const phonetic = phonetics[index];
+      if (phonetic && phonetic.audio) {
+        audioElement.src = phonetic.audio;
+        audioElement.play().catch((error) => {
+          console.error("Ses çalma hatasi:", error);
+        });
+      } else {
+        console.log("ses dosyasi yok");
+      }
+    });
+  });
 }
+
+function setupSynonymButtons() {
+  const anchorBtns = contentArea.querySelectorAll(".synonyms-btn");
+  anchorBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      inputSearch.value = btn.innerText;
+      getFetch();
+      console.log("tiklandi");
+    });
+  });
+}
+
 
 function getFormData() {
   formId.addEventListener("submit", function(e) {
@@ -86,7 +115,7 @@ function getFormData() {
   })
 }
 
-getFormData();
+
 
 
 
@@ -99,7 +128,7 @@ function changeFont() {
 })
 }
 
-changeFont();
+
 
 
 
@@ -109,6 +138,11 @@ function switchTheme() {
   })
 }
 
-switchTheme();
 
 
+function init () {
+  getFetch();
+  getFormData();
+  changeFont();
+  switchTheme();
+}
